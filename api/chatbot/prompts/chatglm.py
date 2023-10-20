eb9d7dfa4f7b89f65bcc1ff71130f07ea17d8516
jlzhou <jameszhou2108@hotmail.com>
2023-10-20 23:50:45 +0800

    styling: unify prompt styling

--- api/chatbot/prompts/chatglm.py
+++ api/chatbot/prompts/chatglm.py
@@ -1,11 +1,17 @@
+"""
+<https://github.com/lm-sys/FastChat/blob/main/fastchat/conversation.py>
+"""
+
 from langchain.prompts import PromptTemplate
 
-template = """{history}
-问: {input}
-答:"""
-prompt = PromptTemplate(input_variables=["history", "input"], template=template)
 
 human_prefix = "问"
 ai_prefix = "答"
 human_suffix = None
 ai_suffix = None
+
+template = f"""{{history}}
+{human_prefix}: {{input}}
+{ai_prefix}:"""
+
+prompt = PromptTemplate(input_variables=["history", "input"], template=template)
--- api/chatbot/prompts/internlm.py
+++ api/chatbot/prompts/internlm.py
@@ -1,12 +1,21 @@
+"""
+<https://github.com/lm-sys/FastChat/blob/main/fastchat/conversation.py>
+<https://huggingface.co/internlm/internlm-chat-7b-8k/blob/bd546fa984b4b0b86958f56bf37f94aa75ab8831/modeling_internlm.py#L771>
+"""
+
 from langchain.prompts import PromptTemplate
 
-template = """
-{history}
-<s><|User|>:{input}<eoh>
-<|Bot|>:"""
-prompt = PromptTemplate(input_variables=["history", "input"], template=template)
 
 human_prefix = "<|User|>"
 ai_prefix = "<|Bot|>"
 human_suffix = "<eoh>"
 ai_suffix = "<eoa>"
+
+template = f"""A chat between a curious <|User|> and an <|Bot|>. The <|Bot|> gives helpful, detailed, and polite answers to the <|User|>'s questions.
+
+
+{{history}}
+<s>{human_prefix}:{{input}}{human_suffix}
+{ai_prefix}:"""
+
+prompt = PromptTemplate(input_variables=["history", "input"], template=template)
