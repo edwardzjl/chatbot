db47d4ef5017420bdc6f10b6e3654d4b9c02585f
Junlin Zhou <jlzhou@zjuici.com>
2023-11-03 21:01:26 +0800

    chore: avoid potential un-aligned behaviour when adding message to CustomRedisChatMessageHistory

--- api/chatbot/history.py
+++ api/chatbot/history.py
@@ -1,7 +1,7 @@
 from uuid import uuid4
 
 from langchain.memory.chat_message_histories import RedisChatMessageHistory
-from langchain.schema import AIMessage, HumanMessage
+from langchain.schema import AIMessage, BaseMessage, HumanMessage
 
 from chatbot.utils import utcnow
 
@@ -11,7 +11,7 @@ class CustomRedisChatMessageHistory(RedisChatMessageHistory):
 
     def add_user_message(self, message: str) -> None:
         """Add an AI message to the store"""
-        self.add_message(
+        super().add_message(
             HumanMessage(
                 content=message,
                 additional_kwargs={
@@ -24,7 +24,7 @@ class CustomRedisChatMessageHistory(RedisChatMessageHistory):
 
     def add_ai_message(self, message: str) -> None:
         """Add an AI message to the store"""
-        self.add_message(
+        super().add_message(
             AIMessage(
                 content=message,
                 additional_kwargs={
@@ -34,3 +34,13 @@ class CustomRedisChatMessageHistory(RedisChatMessageHistory):
                 },
             )
         )
+
+    def add_message(self, message: BaseMessage) -> None:
+        """Append the message to the record in Redis"""
+        additional_info = {
+            "id": uuid4().hex,
+            "sent_at": utcnow().isoformat(),
+            "type": "text",
+        }
+        message.additional_kwargs = additional_info | message.additional_kwargs
+        super().add_message(message)
