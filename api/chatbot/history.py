99ef3837ab0b5babc9e290d02a303bfb386c879c
jlzhou <jameszhou2108@hotmail.com>
2023-11-09 22:12:29 +0800

    chore: simplify code

--- api/chatbot/history.py
+++ api/chatbot/history.py
@@ -1,39 +1,13 @@
 from uuid import uuid4
 
 from langchain.memory.chat_message_histories import RedisChatMessageHistory
-from langchain.schema import AIMessage, BaseMessage, HumanMessage
+from langchain.schema import BaseMessage
 
 from chatbot.utils import utcnow
 
 
 class CustomRedisChatMessageHistory(RedisChatMessageHistory):
     """Persist extra information in `additional_kwargs`."""
-
-    def add_user_message(self, message: str) -> None:
-        """Add an AI message to the store"""
-        super().add_message(
-            HumanMessage(
-                content=message,
-                additional_kwargs={
-                    "id": uuid4().hex,
-                    "sent_at": utcnow().isoformat(),
-                    "type": "text",
-                },
-            )
-        )
-
-    def add_ai_message(self, message: str) -> None:
-        """Add an AI message to the store"""
-        super().add_message(
-            AIMessage(
-                content=message,
-                additional_kwargs={
-                    "id": uuid4().hex,
-                    "sent_at": utcnow().isoformat(),
-                    "type": "text",
-                },
-            )
-        )
 
     def add_message(self, message: BaseMessage) -> None:
         """Append the message to the record in Redis"""
