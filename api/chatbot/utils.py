dcd5ac89a0a2072145bf5e03a6fa3fe4b5c8e837
Junlin Zhou <jlzhou@zjuici.com>
2023-11-30 12:12:21 +0800

    refactor: extract dependencies module

--- api/chatbot/utils.py
+++ api/chatbot/utils.py
@@ -1,15 +1,4 @@
 from datetime import datetime, timezone
-from typing import Optional
-
-from fastapi import Header
-
-from chatbot.config import settings
-
-
-def UserIdHeader(alias: Optional[str] = None, **kwargs):
-    if alias is None:
-        alias = settings.user_id_header
-    return Header(alias=alias, **kwargs)
 
 
 def utcnow():
