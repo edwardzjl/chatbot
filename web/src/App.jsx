3b3a9052f2413edc9065cf0b601342bb3f5e305f
jlzhou <jameszhou2108@hotmail.com>
2023-12-13 22:18:44 +0800

    feat: add blank chatbox header

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -5,6 +5,7 @@ import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 
 import SideMenu from "components/SideMenu";
+import ChatboxHeader from "components/ChatboxHeader";
 import ChatLog from "components/ChatLog";
 import ChatMessage from "components/ChatLog/ChatMessage";
 import ChatInput from "components/ChatInput";
@@ -35,6 +36,7 @@ const App = () => {
     <div className={`App theme-${theme}`}>
       <SideMenu />
       <section className="chatbox">
+        <ChatboxHeader />
         <ChatLog>
           {currentConv && currentConv.messages && currentConv.messages.map((message, index) => (
             <ChatMessage key={index} convId={currentConv.id} idx={index} message={message} />
