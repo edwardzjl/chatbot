4fc3bfc7b58ba9c26a6b02b7fd76445d89668352
jlzhou <jameszhou2108@hotmail.com>
2023-12-17 23:30:23 +0800

    feat: summarize conversation

---
+++ api/chatbot/summarization.py
@@ -0,0 +1,46 @@
+from langchain.chains import LLMChain
+from langchain_core.language_models import BaseLLM
+from langchain_core.memory import BaseMemory
+from langchain_core.prompts import PromptTemplate
+
+_prompt = """<|im_start|>system
+You are Rei, the ideal assistant dedicated to assisting users effectively.<|im_end|>
+{history}
+<|im_start|>system
+Now Provide a short title of the conversation in less than 10 words.<|im_end|>
+<|im_start|>assistant
+"""
+prompt = PromptTemplate(
+    input_variables=["history"],
+    template=_prompt,
+)
+
+
+class SummarizationChain(LLMChain):
+    def prep_outputs(
+        self,
+        inputs: dict[str, str],
+        outputs: dict[str, str],
+        return_only_outputs: bool = False,
+    ) -> dict[str, str]:
+        """Remove the mermory persistence part."""
+        self._validate_outputs(outputs)
+        if return_only_outputs:
+            return outputs
+        else:
+            return {**inputs, **outputs}
+
+
+async def summarize(
+    llm: BaseLLM,
+    memory: BaseMemory,
+) -> str:
+    external_context = memory.load_memory_variables({})
+    chain = SummarizationChain(
+        llm=llm,
+        prompt=prompt,
+        memory=memory,
+        verbose=False,
+    )
+    res = await chain.acall(inputs=external_context)
+    return res["text"]
--- web/src/requests.js
+++ web/src/requests.js
@@ -57,3 +57,14 @@ export const updateConversation = async (conversationId, title) => {
         }),
     })
 };
+
+/**
+ * Generate a summarization of the conversation.
+ * @param {string} conversationId 
+ * @returns {object} {summary: string}
+ */
+export const summarizeConversation = async (conversationId) => {
+    return fetch(`/api/conversations/${conversationId}/summarization`, {
+        method: "POST",
+    }).then(res => res.json());
+}
