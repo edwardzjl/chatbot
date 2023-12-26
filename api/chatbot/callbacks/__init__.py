2f2f159f13de92744108c22560ff91d809225dfa
jlzhou <jameszhou2108@hotmail.com>
2023-12-26 23:39:37 +0800

    feat: introduce llm tracing callback & correctly set log level

--- api/chatbot/callbacks/__init__.py
+++ api/chatbot/callbacks/__init__.py
@@ -1,7 +1,9 @@
 from chatbot.callbacks.conversation import UpdateConversationCallbackHandler
 from chatbot.callbacks.streaming import StreamingLLMCallbackHandler
+from chatbot.callbacks.tracing import TracingLLMCallbackHandler
 
 __all__ = [
     "UpdateConversationCallbackHandler",
     "StreamingLLMCallbackHandler",
+    "TracingLLMCallbackHandler",
 ]
