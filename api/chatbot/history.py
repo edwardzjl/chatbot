154688c3f5bdd41e90dcdda25b22ac5157e42a1e
Junlin Zhou <jlzhou@zjuici.com>
2024-01-05 17:58:12 +0800

    refactor: override RedisChatMessageHistory to store messages by RPUSH

--- api/chatbot/history.py
+++ api/chatbot/history.py
@@ -1,7 +1,8 @@
+import json
 from uuid import uuid4
 
 from langchain_community.chat_message_histories import RedisChatMessageHistory
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, message_to_dict, messages_from_dict
 
 from chatbot.context import session_id
 from chatbot.utils import utcnow
@@ -15,6 +16,14 @@ class ContextAwareMessageHistory(RedisChatMessageHistory):
         """Construct the record key to use"""
         return self.key_prefix + (session_id.get() or self.session_id)
 
+    @property
+    def messages(self) -> list[BaseMessage]:  # type: ignore
+        """Retrieve the messages from Redis"""
+        _items = self.redis_client.lrange(self.key, 0, -1)
+        items = [json.loads(m.decode("utf-8")) for m in _items]
+        messages = messages_from_dict(items)
+        return messages
+
     def add_message(self, message: BaseMessage) -> None:
         """Append the message to the record in Redis"""
         additional_info = {
@@ -23,4 +32,6 @@ class ContextAwareMessageHistory(RedisChatMessageHistory):
             "type": "text",
         }
         message.additional_kwargs = additional_info | message.additional_kwargs
-        super().add_message(message)
+        self.redis_client.rpush(self.key, json.dumps(message_to_dict(message)))
+        if self.ttl:
+            self.redis_client.expire(self.key, self.ttl)
