a1170621d6ddf64ec0b2c2b04c4107431c9627e0
jlzhou <jameszhou2108@hotmail.com>
2024-02-08 22:48:15 +0800

    refactor: persist history in streaming events

--- api/chatbot/chains/conversation.py
+++ api/chatbot/chains/conversation.py
@@ -1,7 +1,4 @@
-from typing import Any
-
 from langchain.chains import LLMChain
-from langchain.memory.chat_memory import BaseChatMemory
 from langchain_core.prompts import (
     BasePromptTemplate,
     HumanMessagePromptTemplate,
@@ -10,7 +7,6 @@ from langchain_core.prompts import (
     SystemMessagePromptTemplate,
 )
 
-from chatbot.memory import ChatbotMemory
 from chatbot.prompts.chatml import ChatMLPromptTemplate
 
 system_prompt = PromptTemplate(
@@ -29,24 +25,10 @@ tmpl = ChatMLPromptTemplate(input_variables=["date", "input"], messages=messages
 
 
 class ConversationChain(LLMChain):
-    """Conversation chain that persists message separately on chain start and end."""
+    """Conversation chain that disables persists messages to memory.
+    I handle the memory saving myself."""
 
     prompt: BasePromptTemplate = tmpl
-    user_input_variable: str
-
-    def prep_inputs(self, inputs: dict[str, Any] | Any) -> dict[str, str]:
-        """Override this method to persist input on chain starts.
-        We need to separatly save the input and output on chain starts and ends.
-        """
-        inputs = super().prep_inputs(inputs)
-        # we need to access the history so we need to ensure it's BaseChatMemory and then we can access it by memory.chat_memory
-        if self.memory is not None and isinstance(self.memory, ChatbotMemory):
-            message = inputs[self.user_input_variable]
-            self.memory.history.add_user_message(message)
-        if self.memory is not None and isinstance(self.memory, BaseChatMemory):
-            message = inputs[self.user_input_variable]
-            self.memory.chat_memory.add_user_message(message)
-        return inputs
 
     def prep_outputs(
         self,
@@ -54,17 +36,8 @@ class ConversationChain(LLMChain):
         outputs: dict[str, str],
         return_only_outputs: bool = False,
     ) -> dict[str, str]:
-        """Override this method to disable saving context to memory.
-        We need to separatly save the input and output on chain starts and ends.
-        """
+        """Override this method to disable saving context to memory."""
         self._validate_outputs(outputs)
-        # we need to access the history so we need to ensure it's BaseChatMemory and then we can access it by memory.chat_memory
-        if self.memory is not None and isinstance(self.memory, ChatbotMemory):
-            text = outputs[self.output_key]
-            self.memory.history.add_ai_message(text)
-        if self.memory is not None and isinstance(self.memory, BaseChatMemory):
-            text = outputs[self.output_key]
-            self.memory.chat_memory.add_ai_message(text)
         if return_only_outputs:
             return outputs
         else:
