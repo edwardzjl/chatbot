2c232e260809da84aface309de7d60fc143f82f7
jlzhou <jameszhou2108@hotmail.com>
2024-03-08 23:01:57 +0800

    feat: implement a very basic welcome component

--- web/src/routes/index/index.jsx
+++ web/src/routes/index/index.jsx
@@ -1,16 +1,23 @@
 import "./index.css";
 
+import { useContext } from "react";
+
+import { UserContext } from "contexts/user";
 import ChatboxHeader from "components/ChatboxHeader";
 
 import ChatInput from "./ChatInput";
 
 
 const Conversation = () => {
-
+    const { username } = useContext(UserContext);
     return (
         <>
             <ChatboxHeader />
-            {/* TODO: add some examples here */}
+            <div className="welcome-container">
+                <div  className="welcome">{username}, hello!</div >
+                <div  className="welcome">How can I help you today?</div >
+            </div>
+            {/* TODO: add some examples here? */}
             <div className="input-bottom">
                 <ChatInput />
                 <div className="footer">Chatbot can make mistakes. Consider checking important information.</div>
