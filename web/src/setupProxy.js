e8dc9e4c3627c0680e53fad8a6e18df8c51caacc
Junlin Zhou <jlzhou@zjuici.com>
2024-04-08 18:41:34 +0800

    build(deps): upgrade http-proxy-middleware to v3

--- web/src/setupProxy.js
+++ web/src/setupProxy.js
@@ -1,7 +1,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const options = {
+const apiProxy = createProxyMiddleware({
   target: "http://127.0.0.1:8000",
+  pathFilter: '/api',
   headers: {
     "X-Forwarded-User": "dev",
     "X-Forwarded-Preferred-Username": "dev",
@@ -9,10 +10,8 @@ const options = {
   },
   changeOrigin: true,
   ws: true,
-};
-
-const apiProxy = createProxyMiddleware("/api", options);
+});
 
 module.exports = function (app) {
-  app.use("/api", apiProxy);
+  app.use(apiProxy);
 };
