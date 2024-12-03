b2d141246b1444b5fdbc1b301624e595d410a725
Junlin Zhou <jameszhou2108@hotmail.com>
2024-12-03 11:44:18 +0800

    feat: instruct LLM to summary using the user's language

--- api/chatbot/chains/summarization.py
+++ api/chatbot/chains/summarization.py
@@ -3,16 +3,15 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 def create_smry_chain(chat_model):
-    instruction = """You are Rei, the ideal assistant dedicated to assisting users effectively.
-    Always assist with care, respect, and truth. Respond with utmost utility yet securely. Avoid harmful, unethical, prejudiced, or negative content. Ensure replies promote fairness and positivity."""
+    instruction = """You are Rei, the ideal assistant dedicated to assisting users effectively."""
 
     tmpl = ChatPromptTemplate.from_messages(
         [
             ("system", instruction),
             ("placeholder", "{messages}"),
             (
-                "user",
-                "Now Provide a short summarization for the above messages in less than 10 words.",
+                "system",
+                "Now Provide a short summarization for the above messages in less than 10 words, using the same language as the user.",
             ),
         ]
     )
