679b2e918b227280e00ae37b143bfc0fd20bb562
Junlin Zhou <jlzhou@zjuici.com>
2024-12-26 09:01:48 +0000

    refactor: refactor error page

--- web/src/routes/error/index.jsx
+++ web/src/routes/error/index.jsx
@@ -1,23 +1,26 @@
-import "./index.css";
+import styles from "./index.module.css";
 
 import { useRouteError, useNavigate } from "react-router-dom";
 
 export default function ErrorPage() {
     const navigate = useNavigate();
-
     const error = useRouteError();
     console.error(error);
 
     const handleClick = () => {
         navigate("/");
-    }
+    };
 
     return (
-        <div id="error-page" className="container">
-            <h1><span>{error.status} {error.statusText || error.message}</span></h1>
-            <h2>Oops!</h2>
-            <p>The page you are looking for does not exist. You can click the button below to go back to the homepage.</p>
-            <button onClick={handleClick}>HOME</button>
+        <div className={styles.container}>
+            <h1 className={styles.errorCode}>
+                <span>{error.status} {error.statusText || error.message}</span>
+            </h1>
+            <h2 className={styles.heading}>Oops! Something went wrong.</h2>
+            <p className={styles.message}>Sorry, we couldn't find the page you are looking for...</p>
+            <button className={styles.homeButton} onClick={handleClick}>
+                HOME
+            </button>
         </div>
     );
 }
