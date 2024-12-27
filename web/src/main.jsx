b85375e051c776fb6b92a600e43ac9b4b1df7a42
Junlin Zhou <jlzhou@zjuici.com>
2024-12-27 16:27:17 +0000

    feat: disable outline for mouse users
    Copied from <https://david-gilbertson.medium.com/removing-that-ugly-focus-ring-and-keeping-it-too-6c8727fefcd2>

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -4,6 +4,26 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+
+const handleFirstTab = (e) => {
+  if (e.keyCode === 9) {
+    document.body.classList.add('user-is-tabbing');
+
+    window.removeEventListener('keydown', handleFirstTab);
+    window.addEventListener('mousedown', handleMouseDownOnce);
+  }
+}
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing');
+
+  window.removeEventListener('mousedown', handleMouseDownOnce);
+  window.addEventListener('keydown', handleFirstTab);
+}
+
+window.addEventListener('keydown', handleFirstTab);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
