059f0f615deaf001fd989fb8831bf3cdaf309c13
Junlin Zhou <jlzhou@zjuici.com>
2024-12-30 16:35:30 +0000

    refactor(web): extract sidebar as component

---
+++ web/src/routes/root/Sidebar/index.jsx
@@ -0,0 +1,75 @@
+import styles from "./index.module.css";
+
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+
+import { ConversationContext } from "@/contexts/conversation";
+
+import ChatTab from "../ChatTab";
+
+
+const Sidebar = ({onShareClick, onDeleteClick}) => {
+    const { groupedConvs } = useContext(ConversationContext);
+
+    return (
+        <aside className={styles.sidebar}>
+            <Link className={styles.sidebarButton} to="/">
+                <AddOutlinedIcon />
+                New Chat
+            </Link>
+            <nav className={styles.convList}>
+                {groupedConvs && Object.entries(groupedConvs)
+                    .filter(([, convs]) => convs && convs.length > 0) // Filter out empty lists
+                    .flatMap(([grp, convs]) => (
+                        [
+                            <div key={grp}>
+                                <div className={styles.sidebarDateGroup}>{grp}</div>
+                                <ul>
+                                    {convs.map((conv) => (
+                                        <li key={conv.id}>
+                                            <NavLink
+                                                to={`conversations/${conv.id}`}
+                                                className={`${styles.sidebarButton} ${({ isActive, isPending }) => isActive ? "active" : isPending ? "pending" : ""}`}
+                                            >
+                                                {({ isActive }) => (
+                                                    <ChatTab chat={conv} isActive={isActive} onShareClick={onShareClick} onDeleteClick={onDeleteClick} />
+                                                )}
+                                            </NavLink>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ]
+                    ))}
+            </nav>
+            <hr className={styles.sidebarBottom} />
+            <div className={styles.sidebarBottomGroup}>
+                <div className={styles.sidebarBottomGroupItem}>
+                    <InfoOutlinedIcon />
+                </div>
+                <div className={styles.sidebarBottomGroupItem}>
+                    <a href="https://github.com/edwardzjl/chatbot" target="_blank" rel="noreferrer"> <GitHubIcon /> </a>
+                </div>
+                <div className={styles.sidebarBottomGroupItem}>
+                    <a href="mailto:jameszhou2108@hotmail.com">
+                        <MailOutlineIcon />
+                    </a>
+                </div>
+            </div>
+        </aside>
+    );
+};
+
+Sidebar.propTypes = {
+    onShareClick: PropTypes.func.isRequired,
+    onDeleteClick: PropTypes.func.isRequired,
+};
+
+
+export default Sidebar;
