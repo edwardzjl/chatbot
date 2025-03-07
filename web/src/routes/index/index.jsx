e7f1219d9094de7410c91573874a4fd6f378c9da
Junlin Zhou <jameszhou2108@hotmail.com>
2025-03-07 18:20:12 +0800

    WIP: add wasm based python sandbox

--- web/src/routes/index/index.jsx
+++ web/src/routes/index/index.jsx
@@ -1,7 +1,8 @@
 import styles from './index.module.css';
 
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { loadPyodide } from "pyodide";
 
 import { UserContext } from "@/contexts/user";
 import { ConversationContext } from "@/contexts/conversation";
@@ -15,6 +16,43 @@ const Conversation = () => {
     const { username } = useContext(UserContext);
     const { dispatch } = useContext(ConversationContext);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const initPyodide = async () => {
+            const pyodide = await loadPyodide({
+                indexURL: "https://cdn.jsdelivr.net/pyodide/v0.27.3/full",
+            });
+            // See <https://pyodide.org/en/stable/usage/file-system.html>
+            // <https://developer.mozilla.org/en-US/docs/Web/API/StorageManager/getDirectory>
+            // <https://developer.mozilla.org/en-US/docs/Web/API/File_System_API/Origin_private_file_system>
+            // quota: <https://developer.mozilla.org/en-US/docs/Web/API/Storage_API/Storage_quotas_and_eviction_criteria>
+            const dirHandle = await navigator.storage.getDirectory();
+            const permissionStatus = await dirHandle.requestPermission({
+                mode: "readwrite",
+            });
+            if (permissionStatus !== "granted") {
+                throw new Error("readwrite access to directory not granted");
+            }
+            // See <https://pyodide.org/en/stable/usage/api/js-api.html#pyodide.mountNativeFS>
+            const nativefs = await pyodide.mountNativeFS("/mount_dir", dirHandle);
+            try {
+                // await pyodide.runPythonAsync(`print("hello noob)`)
+                await pyodide.runPythonAsync(`1 / 0`)
+            } catch (error) {
+                // console.log("type of error", typeof error);  // object
+                // console.log("error keys", Object.keys(error));  // 'type', '__error_address'
+                console.log("error type", error.type);  //
+                // console.log("error address", error.__error_address);  // 0
+                // console.error("Error", error);
+                console.dir(error);
+            }
+            // const res = await pyodide.runPythonAsync("1+1");
+            // console.log("res", res);
+            await nativefs.syncfs();
+        }
+
+        initPyodide();
+    }, []);
 
     const handleSubmit = async (input) => {
         try {
