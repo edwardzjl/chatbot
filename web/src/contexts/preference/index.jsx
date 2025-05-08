a8a1018658889277f6a177b51bc40a22f41d3734
Junlin Zhou <jameszhou2108@hotmail.com>
2025-05-08 17:00:12 +0800

    chore: remove unused context

--- web/src/contexts/preference/index.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-
-export const PreferenceContext = createContext({
-    preference: {
-        forceThinking: false,
-    },
-    setPreference: () => { },
-});
